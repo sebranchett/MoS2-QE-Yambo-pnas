3f10d125cb61b27a0299c9e472094ec431f82259
sebranchett <s.e.branchett@tudelft.nl>
2024-01-29 16:28:48 +0100

    first complete, imperfect BSE calculation

--- bse.sh
+++ bse.sh
@@ -100,24 +100,29 @@ mkdir -p output
 
 # Bethe-Salpeter kernel
 # Create input
-#srun -n1 yambo -o b -k sex -F bse_kernel.in -J BSE
-sed -i 's/BSENGexx.*/BSENGexx=  68              Ry    # [BSK] Exchange components/' bse_kernel.in
-sed -i 's/BSENGBlk.*/BSENGBlk= -1               RL    # [BSK] Screened interaction block size [if -1 uses all the G-vectors of W(q,G,Gp)]/' bse_kernel.in
+# srun -n1 yambo -o b -k sex -F bse_kernel.in -J BSE
+# sed -i 's/BSENGexx.*/BSENGexx=  68              Ry    # [BSK] Exchange components/' bse_kernel.in
+# sed -i 's/BSENGBlk.*/BSENGBlk= -1               RL    # [BSK] Screened interaction block size [if -1 uses all the G-vectors of W(q,G,Gp)]/' bse_kernel.in
 # The article states three valence and five conduction bands
 # Band 52 is the highest occupied state, and states are degenerate
-sed -i 's/.*# \[BSK\] Bands range/  47 |  62 |                     # [BSK] Bands range/' bse_kernel.in
+# sed -i 's/.*# \[BSK\] Bands range/  47 |  62 |                     # [BSK] Bands range/' bse_kernel.in
 # and run
-srun yambo -F bse_kernel.in -J BSE
+# srun yambo -F bse_kernel.in -J BSE
 
 # Reading the QP corrections from a previous GW calculation
 # Create input
 # srun -n1 yambo -F bse_qp.in -y d -V qp -J BSE
-# SEB TODO
-# SEB KfnQPdb= "E < GW_QP_database/ndb.QP"              # [EXTQP BSK BSS] Database
-# SEB KfnQP_INTERP_NN= 1                   # [EXTQP BSK BSS] Interpolation neighbours
-# SEB  % KfnQP_E
-# SEB    0.000000 | 1.000000 | 1.000000 |        # [EXTQP BSK BSS] E parameters  (c/v) eV|adim|adim
-# SEB %
-# and run
+
+# Read the QP corrections from previous GW calculation
+# sed -i 's/KfnQPdb.*/KfnQPdb= "E < output\/gwppa.out\/ndb.QP"  # [EXTQP BSK BSS] Database action/' bse_qp.in
+# write exciton composition, in terms of electron-hole pairs, to disk
+# sed -i 's/#WRbsWF/WRbsWF/' bse_qp.in
+# and run BSE
 # srun yambo -F bse_qp.in -J "output/gwppa.out,BSE"
+gnuplot  <<\EOF
+set terminal png size 500,400
+set output 'bse-5-5-2.png'
+set title 'BSE Optical absorption vs. Energy (eV)'
+plot 'output/o-gwppa.out.eps_q1_diago_bse' u 1:2 w l
+EOF
 
