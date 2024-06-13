91e0bed53d1b39262b04282caff84d21b2d105f3
sebranchett <s.e.branchett@tudelft.nl>
2024-06-13 12:22:03 +0200

    updated 5-5-2 script with working BSE

--- MoS2-5-5-2.sh
+++ MoS2-5-5-2.sh
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=64GB
+#SBATCH --mem-er-cpu=8GB
 
 # find your account with:
 # sacctmgr list -sp user $USER
@@ -146,35 +146,38 @@ EOF
 # Static screening
 # Create input
 srun -n1 yambo -X s -F statscreen.in
-# Change LongDrXs so perturbing electric field has components in each direction
 sed -i 's/NGsBlkXs.*/NGsBlkXs= 5                Ry    # [Xs] Response block size/' statscreen.in
+# set directions for Electric Field - LongDrXs
 sed -i 's/0.000000 |/1.000000 |/g' statscreen.in
 # Run static screening
 srun yambo -F statscreen.in -J BSE
+# Find the number of q-points
+qpoints=$(grep 'IBZ Q-points' r-BSE_screen_dipoles_em1s | awk '{print $4}')
 
-# Bethe-Salpeter kernel
+# Bethe-Salpeter
 # Create input
-srun -n1 yambo -o b -k sex -F bse_kernel.in -J BSE
-sed -i 's/BSENGexx.*/BSENGexx=  68              Ry    # [BSK] Exchange components/' bse_kernel.in
-sed -i 's/BSENGBlk.*/BSENGBlk= -1               RL    # [BSK] Screened interaction block size [if -1 uses all the G-vectors of W(q,G,Gp)]/' bse_kernel.in
+yambo -o b -k sex -y d -F bse.in -J BSE
+sed -i 's/BSENGexx.*/BSENGexx=  68              Ry    # [BSK] Exchange components/' bse.in
+sed -i 's/BSENGBlk.*/BSENGBlk=   5              Ry    # [BSK] Screened interaction block size [if -1 uses all the G-vectors of W(q,G,Gp)]/' bse.in
 # The article states three valence and five conduction bands
 # Band 52 is the highest occupied state, and states are degenerate
-sed -i 's/.*# \[BSK\] Bands range/  47 |  62 |                     # [BSK] Bands range/' bse_kernel.in
-sed -i 's/.*# \[BSK\] Transferred momenta range/ 1 | 10 |                             # [BSK] Transferred momenta range/' bse_kernel.in
-# and run
-srun yambo -F bse_kernel.in -J BSE
-
-# Bethe-Salpeter solver
-# Create input
-srun -n1 yambo -F bse_qp.in -y d -V qp -J BSE
-
-# Reading the QP corrections from a previous GW calculation
-sed -i 's/KfnQPdb.*/KfnQPdb= "E < output\/gwppa.out\/ndb.QP"  # [EXTQP BSK BSS] Database action/' bse_qp.in
+sed -i 's/.*# \[BSK\] Bands range/  47 |  62 |                     # [BSK] Bands range/' bse.in
+sed -i "s/.*# \[BSK\] Transferred momenta range/ 1 | ${qpoints} |                             # [BSK] Transferred momenta range/" bse.in
 # write exciton composition, in terms of electron-hole pairs, to disk
-sed -i 's/#WRbsWF/WRbsWF/' bse_qp.in
-sed -i 's/.*# \[BSK\] Transferred momenta range/ 1 | 10 |                             # [BSK] Transferred momenta range/' bse_qp.in
-# and run BSE
-srun yambo -F bse_qp.in -J "output/gwppa.out,BSE"
+sed -i 's/#WRbsWF/WRbsWF/' bse.in
+# Read the QP corrections from previous GW calculation
+sed -i '/WRbsWF/a KfnQPdb= "E < output\/gwppa.out\/ndb.QP"  # [EXTQP BSK BSS] Database action' bse.in
+# reduce parallelisation memory use
+sed -i '/dipoles/a PAR_def_mode= "memory"           # [PARALLEL] Default distribution mode ("balanced"/"memory"/"workload"/"KQmemory")' bse.in
+
+# add a line to remove the line below (limitation of sed?)
+sed -i "/% BLongDir/a remove this line and the next" bse.in
+sed -i "/remove this line and the next/,+1d" bse.in
+# replace the removed line
+sed -i "/% BLongDir/a \ 1.000000 | 1.000000 | 1.000000 |        # [BSS] [cc] Electric Field" bse.in
+
+# Run Bethe-Salpeter
+srun yambo -F bse.in -J "output/gwppa.out,BSE"
 
 # Plot the Optical Absorption
 gnuplot  <<\EOF
